60c712b3945c0ad9a16461a7ca14204800c71a51
Diarcip <diegocifuentes98@hotmail.com>
2021-05-20 19:40:41 -0500

    "Navbar responsive finished"

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function MenuClick() {
+  document
+    .getElementById("navigation-bar")
+    .classList.add("navigation-bar-visible");
+}
+function ExitClick() {
+  document
+    .getElementById("navigation-bar")
+    .classList.remove("navigation-bar-visible");
+}
