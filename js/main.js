7b68df38ecedd80baedb9487721dda25bca5cb13
Diarcip <diegocifuentes98@hotmail.com>
2021-06-12 20:40:35 -0500

    Se cambio la estructura del navbar y como funcionaba en dispositivos moviles reduciendo la cantidad de lineas.  Se redujo el tamaño del site para menor tiempo de carga

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 function MenuClick() {
-  document
-    .getElementById("navigation-bar")
-    .classList.add("navigation-bar-visible");
-}
-function ExitClick() {
-  document
-    .getElementById("navigation-bar")
-    .classList.remove("navigation-bar-visible");
+  let menuBox = document.getElementById("nav-box");
+  let boxHasClass = menuBox.classList.contains("nav-box-visible");
+  let menuButton = document.getElementById("burger-icon");
+
+  if (!boxHasClass) {
+    menuBox.classList.add("nav-box-visible");
+    menuButton.classList.add("fa-times");
+    menuButton.classList.remove("fa-bars");
+  } else {
+    menuBox.classList.remove("nav-box-visible");
+    menuButton.classList.remove("fa-times");
+    menuButton.classList.add("fa-bars");
+  }
 }
